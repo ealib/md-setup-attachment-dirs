d38dd51223703ee59d0144dbeb00030b31f99afc
Emanuele Aliberti <dev@mtka.eu>
2024-07-26 11:19:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# md-setup-attachment-dirs
+
+> WARNING: This is script operates on each MDaemon user's mailbox.
+
+Script for [MDaemon](https://mdaemon.com/) to **force change** the
+**folder type** to _document_ in
+each folder that matches
+
+    {{USER_MAILBOX_DIR}}\DOCUMENTS.IMAP\X-ATTACHMENTS.IMAP\{{SENDER}}.IMAP\Hiwater.mrk
+
+Search if `Hiwater.mrk` database exist, or create a new one, and
+set `/Groupware/FolderClass` to `IPF.Document`:
+
+```ini
+[Groupware]
+FolderClass=IPF.Document
+```
+
+## Installation
+
+Prerequisites: [Node.js](https://nodejs.org/)
+
+```dos
+C:\>node --version
+v20.16.0
+
+C:\>
+```
+
+Git clone this repository, or download a source bundle (Code &rarr; Download ZIP):
+
+```dos
+C:\>git clone https://github.com/ealib/md-setup-attachment-dirs.git
+```
+
+Install all script dependencies:
+
+```dos
+C:\>cd md-setup-attachment-dirs
+
+C:\md-setup-attachment-dirs>yarn install
+```
+
+## Usage
+
+```dos
+C:\md-setup-attachment-dirs>yarn start
+yarn run v1.22.22
+$ node index.js
+
+MDaemon path    : C:\MDaemon\    
+MDaemon app path: C:\MDaemon\APP\
+MDaemon version : 24.0.1        
+
+Scanning user mailboxes:
+
+(1) MDaemon Server <MDaemon@example.com>
+        mailbox    : C:\MDaemon\Users\example.com\MDaemon
+
+(2) Example User <example.user@example.com>
+        mailbox    : C:\MDaemon\Users\example.com\example.user
+        attachments: C:\MDaemon\Users\example.com\example.user\Documents.IMAP\X-ATTACHMENTS.IMAP
+                 bob@acme.com
+                         C:\MDaemon\Users\example.com\example.user\Documents.IMAP\X-ATTACHMENTS.IMAP\bob@acme.com.IMAP\Hiwater.mrk
+
+(3) user <user@example.com>
+        mailbox    : C:\MDaemon\Users\example.com\user
+
+Done in 0.46s.
+
+C:\md-setup-attachment-dirs>
+```
+
+## Dependencies
+
+- [chalk](https://www.npmjs.com/package/chalk)
+- [fs-extra](https://www.npmjs.com/package/fs-extra)
+- [ini](https://www.npmjs.com/package/ini)
+- [node-mdaemon-api](https://www.npmjs.com/package/node-mdaemon-api)
+
+## Legal disclaimer
+
+MDaemon® is a trademark of [MDaemon Technologies, Ltd.](https://mdaemon.com/pages/about-us)
+MDaemon Technologies makes no representations, endorsements, or
+warranties regarding Third Party Products or Services.
+
+Node.js is a trademark of [OpenJS Foundation](https://openjsf.org/).
+
+Windows&trade; is a [trademark of Microsoft Corp.](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks)
+
+## License
+
+`md-setup-attachment-dirs` is [MIT licensed](LICENSE.md).
---
+++ index.js
@@ -0,0 +1,109 @@
+// In all "Hiwater.mrk" databases that match
+//
+// $APP_PATH$\USERS\$RECIPIENTDOMAIN$\$RECIPIENTMAILBOX$\DOCUMENTS.IMAP\X-ATTACHMENTS.IMAP\$SENDER$.IMAP\Hiwater.mrk
+//
+// force /Groupware/FolderClass to "IPF.Document"
+//
+// [Groupware]
+// FolderClass=IPF.Document
+
+'require strict'
+
+import chalk from 'chalk';
+import fs from 'fs-extra';
+import { stringify, parse } from 'ini';
+import { createRequire } from 'module';
+
+// Native modules can not be import-ed
+const require = createRequire(import.meta.url);
+const md = require('node-mdaemon-api');
+
+const DOCUMENTS_IMAP = 'Documents.IMAP';
+const X_ATTACHMENTS_IMAP = 'X-ATTACHMENTS.IMAP';
+
+const HIWATER_MRK = 'Hiwater.mrk';
+
+const INI_SEC_GROUPWARE = 'Groupware';
+const INI_KEY_FOLDER_CLASS = 'FolderClass';
+const INI_VALUE_IPF_DOCUMENT = 'IPF.Document';
+
+// entry point
+
+if (!md.isReady) {
+    throw new Error('MDaemon is not ready, or it is not installed!');
+}
+
+const mdInfo = md.getMdInfo()
+const mdAppPath = md.mdAppPath;
+const mdPath = mdAppPath.split('\\').filter(segment => segment.toUpperCase() !== 'APP').join('\\');
+
+console.log();
+console.log('MDaemon path    :', chalk.yellow(mdPath));
+console.log('MDaemon app path:', chalk.yellow(mdAppPath));
+console.log('MDaemon version :', chalk.yellow(mdInfo.version.full));
+console.log();
+
+console.log('Scanning user mailboxes:');
+
+md.readUsersSync().forEach((user, index) => {
+    console.log();
+    console.log(`(${1 + index})`, chalk.green(user.FullName), `<${user.Email}>`);
+
+    const userHandle = md.MD_GetByEmail(user.Email);
+    const userInfo = md.MD_GetUserInfo(userHandle);
+    const mailDir = userInfo.MailDir.endsWith('\\') ? userInfo.MailDir.slice(0, -1) : userInfo.MailDir;
+    md.MD_GetFree(userHandle);
+
+    console.log('\tmailbox    :', mailDir);
+
+    if (!fs.existsSync(mailDir)) return;
+
+    const documentDir = [mailDir, DOCUMENTS_IMAP].join('\\');
+    if (!fs.existsSync(documentDir)) return;
+
+    const attachmentsBaseDir = [mailDir, DOCUMENTS_IMAP, X_ATTACHMENTS_IMAP].join('\\');
+    if (!fs.existsSync(attachmentsBaseDir)) return;
+
+    // OK, attachments directory exists
+    console.log('\tattachments:', attachmentsBaseDir);
+    const senderDirs = fs.readdirSync(attachmentsBaseDir);
+    if (senderDirs.length === 0) {
+        console.log(chalk.yellow('\t\tno senders'));
+    } else {
+        // at least one sender directory:
+        // we exclude directories that are not IMAP folders for MD
+        senderDirs
+            .filter(sender => sender.toUpperCase().endsWith('.IMAP'))
+            .forEach(sender => {
+                console.log('\t\t', sender.slice(0, -5));
+                const hiwaterMrk = [mailDir, DOCUMENTS_IMAP, X_ATTACHMENTS_IMAP, sender, HIWATER_MRK].join('\\');
+                console.log('\t\t\t', hiwaterMrk);
+
+                let db = {};
+                if (fs.existsSync(hiwaterMrk)) {
+                    const content = fs.readFileSync(hiwaterMrk, {
+                        encoding: 'utf-8'
+                    });
+                    db = parse(content);
+                    if (!db[INI_SEC_GROUPWARE]) {
+                        db[INI_SEC_GROUPWARE] = {};
+                    }
+                    if (!db[INI_SEC_GROUPWARE][INI_KEY_FOLDER_CLASS]) {
+                        db[INI_SEC_GROUPWARE][INI_KEY_FOLDER_CLASS] = {};
+                    }
+                    db[INI_SEC_GROUPWARE][INI_KEY_FOLDER_CLASS] = INI_VALUE_IPF_DOCUMENT;
+                } else {
+                    db = {
+                        [INI_SEC_GROUPWARE]: {
+                            [INI_KEY_FOLDER_CLASS]: INI_VALUE_IPF_DOCUMENT,
+                        },
+                    };
+                }
+                // Save the created/updated Hiwater.mrk
+                const ini = stringify(db);
+                fs.writeFileSync(hiwaterMrk, ini);
+            });
+    }
+});
+
+console.log();
